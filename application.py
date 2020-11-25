36324059e2f4c9b7714cc005a51660dcce7dbabb
rwarnking <rwarnking@gmail.com>
2020-11-25 17:59:33 +0100

    Added detailed view, fixed a bug, new algorithm

--- application.py
+++ application.py
@@ -27,26 +27,27 @@ def browse_button(dir, initial):
 source_dir = StringVar()
 source_dir.set(S_DIR)
 source_button = Button(window, text="Browse for source directory.", command=lambda: browse_button(source_dir, S_DIR))
-source_button.grid(row=0, column=0, pady = 10)
+source_button.grid(row=0, column=0, pady = (10, 0))
 lbl1 = Label(window, textvariable=source_dir)
-lbl1.grid(row=1, column=0)
+lbl1.grid(row=1, column=0, pady = (10, 0))
 
 replacement_dir = StringVar()
 replacement_dir.set(R_DIR)
 replacement_button = Button(window, text="Browse for replacement directory.", command=lambda: browse_button(replacement_dir, R_DIR))
-replacement_button.grid(row=2, column=0, pady = 10)
+replacement_button.grid(row=2, column=0, pady = (10, 0))
 lbl1 = Label(window, textvariable=replacement_dir)
-lbl1.grid(row=3, column=0)
+lbl1.grid(row=3, column=0, pady = 10)
 
 # TODO add target folder
 
 # Create checkboxes
+# TODO give option to select block amount
 water_blocks = IntVar()
-Checkbutton(window, text="water_blocks", variable=water_blocks).grid(row=4, sticky=W, padx = 20)
+Checkbutton(window, text="Fix pockets in water.", variable=water_blocks).grid(row=4, sticky=W, padx = 20)
 air_blocks = IntVar()
-Checkbutton(window, text="air_blocks", variable=air_blocks).grid(row=5, sticky=W, padx = 20)
+Checkbutton(window, text="Fix air pockets.", variable=air_blocks).grid(row=5, sticky=W, padx = 20)
 new_blocks = IntVar()
-Checkbutton(window, text="new_blocks", variable=new_blocks).grid(row=6, sticky=W, padx = 20)
+Checkbutton(window, text="Use replacement blocks from other map.", variable=new_blocks).grid(row=6, sticky=W, padx = 20)
 
 # Progress bar widget
 chunk_progress = Progressbar(window, orient = HORIZONTAL, length = 300, mode = 'determinate')
@@ -65,8 +66,30 @@ chunk_label.grid(row=8, sticky=W, padx = 20)
 files_label = Label(window, text="")
 files_label.grid(row=10, sticky=W, padx = 20)
 
-# Create Buttons
-run_button = Button(window, text="Dew it", command=lambda: run(source_dir.get(), chunk_progress, file_progress, chunk_label, files_label, water_blocks, air_blocks, new_blocks))
+# Create details button
+def details():
+    if details_text.hidden:
+        details_text.grid()
+        details_text.hidden = False
+        run_button.grid(row=12)
+    else:
+        details_text.grid_remove()
+        details_text.hidden = True
+        run_button.grid(row=11)
+
+details_button = Button(window, text="Details", command=details)
+details_button.grid(row=10, column=0, sticky=E, padx = 20, pady = 10)
+
+# Details Menu
+details_text = Text(window, height=10, width=35)
+details_scroll = Scrollbar(window, command=details_text.yview)
+details_text.configure(yscrollcommand=details_scroll.set)
+details_text.grid(row=11, column=0, padx = 20, pady = 10)
+details_text.grid_remove()
+details_text.hidden = True
+
+# Create run button
+run_button = Button(window, text="Dew it", command=lambda: run(source_dir.get(), chunk_progress, file_progress, chunk_label, files_label, water_blocks.get(), air_blocks.get(), new_blocks.get(), details_text))
 run_button.grid(row=11, column=0, padx = 20, pady = 10)
 
 window.mainloop()
--- modifier.py
+++ modifier.py
@@ -4,6 +4,11 @@ import anvil
 # Needed for the file iteration
 import os
 
+# For array manipulations
+import numpy as np
+
+from tkinter import END
+
 # utilities
 from math import floor
 from time import gmtime, strftime
@@ -78,22 +83,151 @@ def updateProgressBar(progress, value):
 ###################################################################################################
 # Checks
 ###################################################################################################
-def solidNeighbours(chunk, x, y, z):
+def checkNeighbours(chunk, x, y, z, blockType):
     for i in range(x - 1, x + 2):
         for j in range(y - 1, y + 2):
             for k in range(z - 1, z + 2):
-                if (not (x == i and y == j and z == k)
-                    and i >= 0 and j >= 0 and k >= 0
-                    and i < 16 and j < 256 and k < 16):
+                if (i <= 0 or j <= 0 or k <= 0
+                    or i >= 15 or j >= 255 or k >= 15):
+                    return False
+                if not (x == i and y == j and z == k):
                     block = chunk.get_block(i, j, k)
-                    if block.id == 'air':
+                    if block.id == blockType:
                         return False
     return True
+
+# TODO multiple types should be possible
+# TODO this should be a pocket search similar to how the air pockets should be found
+# TODO the marking mechanism does not work because the stateArray is only changed for one block
+def checkWaterBlocks(chunk, block, x, y, z):
+    if block.id == 'dirt':
+        return checkNeighbours(chunk, x, y, z, 'dirt')
+    return False
+
+# TODO
+def checkAirPockets(chunk, block, x, y, z):
+    if block.id == 'air':
+        return checkNeighbours(chunk, x, y, z, 'air')
+    return False
+
+# TODO test not only for stone and air
+def checkSolidArea(chunk, block, x, y, z):
+    if block.id == 'stone':
+        return checkNeighbours(chunk, x, y, z, 'air')
+    return False
 
 ###################################################################################################
 # Copy
 ###################################################################################################
-def copyChunk(newRegion, region, chunkX, chunkZ):
+UNCHECKED = 0
+UNCHANGED = 1
+WATERBLOCK = 2
+AIRPOCKET = 3
+AIRPOCKET_STONE = 4
+SOLIDAREA = 5
+
+changeCountWater = 0
+changeCountAir = 0
+changeCountSolid = 0
+
+def copyChunk(newRegion, region, chunkX, chunkZ, water_blocks, air_pockets, solid_blocks):
+    # TODO make try block smaller
+    try:
+        chunk = anvil.Chunk.from_region(region, chunkX, chunkZ)
+
+        # Init
+        stateArray = np.zeros((16, 256, 16))
+        x = 0
+        y = 0
+        z = 0
+
+        # Check how the chunk should be modified and save it in the stateArray
+        for block in chunk.stream_chunk():
+            if stateArray[x, y, z] == UNCHECKED:
+                if water_blocks == 1 and checkWaterBlocks(chunk, block, x, y, z):
+                    stateArray[x, y, z] = WATERBLOCK
+                    changeCountWater += 1
+                elif air_pockets == 1 and solid_blocks == 1 and checkAirPockets(chunk, block, x, y, z):
+                    stateArray[x, y, z] = AIRPOCKET
+                    changeCountAir += 1
+                elif air_pockets == 1 and checkAirPockets(chunk, block, x, y, z):
+                    stateArray[x, y, z] = AIRPOCKET_STONE
+                    changeCountAir += 1
+                elif solid_blocks == 1 and checkSolidArea(chunk, block, x, y, z):
+                    stateArray[x, y, z] = SOLIDAREA
+                    changeCountSolid += 1
+                else:
+                    stateArray[x, y, z] = UNCHANGED
+
+            if z == 15 and x == 15:
+                y += 1
+            if x == 15:
+                z = (z + 1) % 16
+            x = (x + 1) % 16
+
+        # Reset
+        newChunk = 0
+        x = 0
+        y = 0
+        z = 0
+
+        # Create `Block` objects that are used to set blocks
+        stone = anvil.Block('minecraft', 'stone')
+        water = anvil.Block('minecraft', 'water')
+        dirt = anvil.Block('minecraft', 'dirt')
+
+        # Iterate all blocks and select write the new block to the newChunk
+        for block in chunk.stream_chunk():
+            b = block
+
+            if stateArray[x, y, z] == WATERBLOCK:
+                b = water
+            # elif stateArray[x, y, z] == AIRPOCKET:
+            #     newblock = replChunk.get_block(i, j, k)
+            #     if newBlock.id == Solid:
+            #         b = newBlock
+            #     else:
+            #         b = stone
+            elif stateArray[x, y, z] == AIRPOCKET_STONE:
+                b = stone
+            # elif stateArray[x, y, z] == SOLIDAREA:
+            #     newBlock = replChunk.get_block(i, j, k)
+            #     if newBlock.id == Solid:
+            #         b = newBlock
+            #     else:
+            #         b = stone
+
+            try:
+                newChunk = newRegion.set_block(b, newRegion.x * 512 + chunkX * 16 + x, y, newRegion.z * 512 + chunkZ * 16 + z)
+            except:
+                print(f'could not set Block ({x},{y},{z})')
+
+            if z == 15 and x == 15:
+                y += 1
+            if x == 15:
+                z = (z + 1) % 16
+            x = (x + 1) % 16
+
+        newChunk.set_data(chunk.data)
+
+        #print(f'finished chunk ({chunkX},{chunkZ})')
+    except:
+        print(f'skipped non-existent chunk ({chunkX},{chunkZ})')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def copyChunk2(newRegion, region, chunkX, chunkZ):
     try:
         chunk = anvil.Chunk.from_region(region, chunkX, chunkZ)
 
@@ -130,13 +264,13 @@ def copyChunk(newRegion, region, chunkX, chunkZ):
 
         newChunk.set_data(chunk.data)
 
-        print(f'finished chunk ({chunkX},{chunkZ})')
+        #print(f'finished chunk ({chunkX},{chunkZ})')
     except:
         print(f'skipped non-existent chunk ({chunkX},{chunkZ})')
 
 ###################################################################################################
 
-def copyRegion(chunk_progress, chunk_label, src_dir, target_dir, filename):
+def copyRegion(chunk_progress, chunk_label, src_dir, target_dir, filename, water_blocks, air_pockets, solid_blocks):
     l = filename.split('.')
     rX = int(l[1])
     rZ = int(l[2])
@@ -149,26 +283,23 @@ def copyRegion(chunk_progress, chunk_label, src_dir, target_dir, filename):
     print(src_dir + "/" + filename)
     region = anvil.Region.from_file(src_dir + "/" + filename)
 
-    # Create `Block` objects that are used to set blocks
-    # stone = anvil.Block('minecraft', 'stone')
-    dirt = anvil.Block('minecraft', 'dirt')
-
-    change_count = 0
-
-    max_chunkX = 3
-    max_chunkZ = 3
+    max_chunkX = 32
+    max_chunkZ = 32
     chunk_progress["maximum"] = max_chunkX * max_chunkZ
     chunk_label.config(text=f"Finished chunk 0, 0 of {max_chunkX - 1}, {max_chunkZ - 1}. And 0 of {max_chunkX * max_chunkZ} chunks.")
 
     for chunkX in range(max_chunkX):
         for chunkZ in range(max_chunkZ):
 
-            copyChunk(newRegion, region, chunkX, chunkZ)
+            copyChunk(newRegion, region, chunkX, chunkZ, water_blocks, air_pockets, solid_blocks)
 
             updateProgressBar(chunk_progress, chunkZ + 1 + chunkX * max_chunkZ)
             chunk_label.config(text=f"Finished chunk {chunkX}, {chunkZ} of {max_chunkX - 1}, {max_chunkZ - 1}. And {chunkZ + 1 + chunkX * max_chunkZ} of {max_chunkX * max_chunkZ} chunks.")
 
-    print(f'Changed {change_count} blocks')
+    print(f'In file {filename}:')
+    print(f'Changed {changeCountWater} solid blocks to water.')
+    print(f'Changed {changeCountAir} plocks to solid blocks.')
+    print(f'Changed {changeCountSolid} solid blocks to replacement solid blocks.')
 
     # Save to a file
     #path = 'original_copy/'
@@ -181,26 +312,29 @@ def copyRegion(chunk_progress, chunk_label, src_dir, target_dir, filename):
 def run(src_dir,
     chunk_progress, file_progress,
     chunk_label, files_label,
-    water_blocks, air_blocks, new_blocks):
+    water_blocks, air_pockets, solid_blocks,
+    details_text):
 
-    if (water_blocks.get() == 1):
-        print("Water Blocks are getting fixed!")
+    # Print detailed informations
+    if (water_blocks == 1):
+        details_text.insert(END, "Water Blocks will be fixed!\n")
     else:
-        print("Water Blocks are not getting fixed!")
+        details_text.insert(END, "Water Blocks will not be fixed!\n")
 
-    if (air_blocks.get() == 1):
-        print("Air Blocks are getting fixed!")
+    if (air_pockets == 1):
+        details_text.insert(END, "Air Blocks will be fixed!\n")
     else:
-        print("Air Blocks are not getting fixed!")
+        details_text.insert(END, "Air Blocks will not be fixed!\n")
 
-    if (new_blocks.get() == 1):
-        print("New Blocks are getting fixed!")
+    if (solid_blocks == 1):
+        details_text.insert(END, "New Blocks will be fixed!\n")
     else:
-        print("New Blocks are not getting fixed!")
+        details_text.insert(END, "New Blocks will not be inserted!\n")
 
-    print('.. starting')
-    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    details_text.insert(END, "\n.. starting\n")
+    details_text.insert(END, strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
+    # Get all files in the directory
     filelist = os.listdir(src_dir)
     if len(filelist) == 0:
         print("No files found! Select a different source path.")
@@ -212,21 +346,22 @@ def run(src_dir,
             os.mkdir(target_dir)
     except OSError:
         print ("Creation of the directory %s failed" % target_dir)
-    else:
-        print ("Successfully created the directory %s " % target_dir)
 
+    # Update the progressbar and label for the files
     file_progress["maximum"] = len(filelist)
     files_label.config(text=f"Finished file {0} of {len(filelist)} files.")
 
+    # Iterate the files
     i = 1
     for filename in filelist:
         if filename.endswith(".mca"):
-            copyRegion(chunk_progress, chunk_label, src_dir, target_dir, filename)
+            copyRegion(chunk_progress, chunk_label, src_dir, target_dir, filename, water_blocks, air_pockets, solid_blocks)
         else:
             continue
         updateProgressBar(file_progress, i)
         files_label.config(text=f"Finished file {i} of {len(filelist)} files.")
         i += 1
 
-    print('.. finished')
-    print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    # Print that the process is finished
+    details_text.insert(END, "\n.. finished\n")
+    details_text.insert(END, strftime("%Y-%m-%d %H:%M:%S", gmtime()))
