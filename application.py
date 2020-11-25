36f9ffc7951b41a95defe869ae3929793cd78724
rwarnking <rwarnking@gmail.com>
2020-11-25 14:45:21 +0100

    Basic path selection, second progbar, multifile

--- application.py
+++ application.py
@@ -13,20 +13,29 @@ window = Tk()
 # Set the title
 window.title("mc-map-modifier")
 
-def browse_button(dir):
-    filename = filedialog.askdirectory(initialdir = "C:/Users/"+os.getlogin()+"/AppData/Roaming/.minecraft/saves")
+# Test path
+S_DIR = "C:/Users/"+os.getlogin()+"/Projekte/mc-map-modifier/original"
+R_DIR = "C:/Users/"+os.getlogin()+"/Projekte/mc-map-modifier/original_replacement"
+
+#S_DIR = "C:/Users/"+os.getlogin()+"/AppData/Roaming/.minecraft/saves"
+#R_DIR = "C:/Users/"+os.getlogin()+"/AppData/Roaming/.minecraft/saves"
+
+def browse_button(dir, initial):
+    filename = filedialog.askdirectory(initialdir = initial)
     dir.set(filename)
 
 source_dir = StringVar()
-source_button = Button(window, text="Browse for source directory.", command=lambda: browse_button(source_dir))
-source_button.grid(row=0, column=0)
+source_dir.set(S_DIR)
+source_button = Button(window, text="Browse for source directory.", command=lambda: browse_button(source_dir, S_DIR))
+source_button.grid(row=0, column=0, pady = 10)
 lbl1 = Label(window, textvariable=source_dir)
 lbl1.grid(row=1, column=0)
 
-replacment_dir = StringVar()
-replacment_button = Button(window, text="Browse for replacment directory.", command=lambda: browse_button(replacment_dir))
-replacment_button.grid(row=2, column=0)
-lbl1 = Label(window, textvariable=replacment_dir)
+replacement_dir = StringVar()
+replacement_dir.set(R_DIR)
+replacement_button = Button(window, text="Browse for replacement directory.", command=lambda: browse_button(replacement_dir, R_DIR))
+replacement_button.grid(row=2, column=0, pady = 10)
+lbl1 = Label(window, textvariable=replacement_dir)
 lbl1.grid(row=3, column=0)
 
 # TODO add target folder
@@ -40,19 +49,24 @@ new_blocks = IntVar()
 Checkbutton(window, text="new_blocks", variable=new_blocks).grid(row=6, sticky=W, padx = 20)
 
 # Progress bar widget
-progress = Progressbar(window, orient = HORIZONTAL, length = 300, mode = 'determinate')
-progress["value"] = 0
-progress.update()
-progress.grid(row=7, sticky=W, padx = 20, pady = 10)
+chunk_progress = Progressbar(window, orient = HORIZONTAL, length = 300, mode = 'determinate')
+chunk_progress["value"] = 0
+chunk_progress.update()
+chunk_progress.grid(row=7, sticky=W, padx = 20, pady = 10)
+
+file_progress = Progressbar(window, orient = HORIZONTAL, length = 300, mode = 'determinate')
+file_progress["value"] = 0
+file_progress.update()
+file_progress.grid(row=9, sticky=W, padx = 20, pady = 10)
 
 # Progress label
 chunk_label = Label(window, text="")
 chunk_label.grid(row=8, sticky=W, padx = 20)
 files_label = Label(window, text="")
-files_label.grid(row=9, sticky=W, padx = 20)
+files_label.grid(row=10, sticky=W, padx = 20)
 
 # Create Buttons
-run_button = Button(window, text="Dew it", command=lambda: run(progress, chunk_label, files_label, water_blocks, air_blocks, new_blocks))
-run_button.grid(row=10, column=0, padx = 20, pady = 10)
+run_button = Button(window, text="Dew it", command=lambda: run(source_dir.get(), chunk_progress, file_progress, chunk_label, files_label, water_blocks, air_blocks, new_blocks))
+run_button.grid(row=11, column=0, padx = 20, pady = 10)
 
 window.mainloop()
--- modifier.py
+++ modifier.py
@@ -1,6 +1,9 @@
 # minecraft import
 import anvil
 
+# Needed for the file iteration
+import os
+
 # utilities
 from math import floor
 from time import gmtime, strftime
@@ -69,10 +72,8 @@ def printRegion(filename):
 # Update method
 ###################################################################################################
 def updateProgressBar(progress, value):
-    import time
     progress['value'] = value
     progress.update()
-    time.sleep(1)
 
 ###################################################################################################
 # Checks
@@ -92,14 +93,61 @@ def solidNeighbours(chunk, x, y, z):
 ###################################################################################################
 # Copy
 ###################################################################################################
-def copyRegion(progress, chunk_label, rX, rZ):
+def copyChunk(newRegion, region, chunkX, chunkZ):
+    try:
+        chunk = anvil.Chunk.from_region(region, chunkX, chunkZ)
+
+        # newChunk = newRegion.add_chunk(anvil.EmptyChunk(chunkX, chunkZ))
+        newChunk = 0
+
+        x = 0
+        y = 0
+        z = 0
+
+        index = 0
+        for block in chunk.stream_chunk():
+            b = block
+            # print(f'{block.id} - ({x},{y},{z}) - {index}')
+            # TODO: compute coordinates from chunk and index
+            # TODO this wants global coordinates but gets region coords
+            # if block.id == 'air' and solidNeighbours(chunk, x, y, z):
+            #     b = dirt
+            #     print(f'Changed: {x} {y} {z}')
+            #     change_count += 1
+
+
+            try:
+                newChunk = newRegion.set_block(b, chunkX * 16 + x, y, chunkZ * 16 + z)
+            except:
+                print(f'could not set Block ({x},{y},{z})')
+            index += 1
+
+            if z == 15 and x == 15:
+                y += 1
+            if x == 15:
+                z = (z + 1) % 16
+            x = (x + 1) % 16
+
+        newChunk.set_data(chunk.data)
+
+        print(f'finished chunk ({chunkX},{chunkZ})')
+    except:
+        print(f'skipped non-existent chunk ({chunkX},{chunkZ})')
+
+###################################################################################################
+
+def copyRegion(chunk_progress, chunk_label, src_dir, target_dir, filename):
+    l = filename.split('.')
+    rX = int(l[1])
+    rZ = int(l[2])
+
     # Create a new region with the `EmptyRegion` class at 0, 0 (in region coords)
     newRegion = anvil.EmptyRegion(rX, rZ)
 
-    path = 'original/'
-    filename = 'r.'+str(rX)+'.'+str(rZ)+'.mca'
-    print(path + filename)
-    region = anvil.Region.from_file(path + filename)
+    #path = 'original/'
+    #filename = 'r.'+str(rX)+'.'+str(rZ)+'.mca'
+    print(src_dir + "/" + filename)
+    region = anvil.Region.from_file(src_dir + "/" + filename)
 
     # Create `Block` objects that are used to set blocks
     # stone = anvil.Block('minecraft', 'stone')
@@ -109,65 +157,31 @@ def copyRegion(progress, chunk_label, rX, rZ):
 
     max_chunkX = 3
     max_chunkZ = 3
-    progress["maximum"] = max_chunkX * max_chunkZ
+    chunk_progress["maximum"] = max_chunkX * max_chunkZ
+    chunk_label.config(text=f"Finished chunk 0, 0 of {max_chunkX - 1}, {max_chunkZ - 1}. And 0 of {max_chunkX * max_chunkZ} chunks.")
 
     for chunkX in range(max_chunkX):
         for chunkZ in range(max_chunkZ):
 
-            try:
-                chunk = anvil.Chunk.from_region(region, chunkX, chunkZ)
-                # newChunk = newRegion.add_chunk(anvil.EmptyChunk(chunkX, chunkZ))
-                newChunk = 0
+            copyChunk(newRegion, region, chunkX, chunkZ)
 
-                x = 0
-                y = 0
-                z = 0
-
-                index = 0
-                for block in chunk.stream_chunk():
-                    b = block
-                    # print(f'{block.id} - ({x},{y},{z}) - {index}')
-                    # TODO: compute coordinates from chunk and index
-                    # TODO this wants global coordinates but gets region coords
-                    # if block.id == 'air' and solidNeighbours(chunk, x, y, z):
-                    #     b = dirt
-                    #     print(f'Changed: {x} {y} {z}')
-                    #     change_count += 1
-
-
-                    try:
-                        newChunk = newRegion.set_block(b, chunkX * 16 + x, y, chunkZ * 16 + z)
-                    except:
-                        print(f'could not set Block ({x},{y},{z})')
-                    index += 1
-
-                    if z == 15 and x == 15:
-                        y += 1
-                    if x == 15:
-                        z = (z + 1) % 16
-                    x = (x + 1) % 16
-
-                newChunk.set_data(chunk.data)
-
-                print(f'finished chunk ({chunkX},{chunkZ})')
-
-                updateProgressBar(progress, chunkZ + 1 + chunkX * max_chunkZ)
-                chunk_label.config(text=f"Finished chunk {chunkX}, {chunkZ} of {max_chunkX - 1}, {max_chunkZ - 1}. And {chunkZ + 1 + chunkX * max_chunkZ} of {max_chunkX * max_chunkZ} chunks.")
-
-            except:
-                print(f'skipped non-existent chunk ({chunkX},{chunkZ})')
+            updateProgressBar(chunk_progress, chunkZ + 1 + chunkX * max_chunkZ)
+            chunk_label.config(text=f"Finished chunk {chunkX}, {chunkZ} of {max_chunkX - 1}, {max_chunkZ - 1}. And {chunkZ + 1 + chunkX * max_chunkZ} of {max_chunkX * max_chunkZ} chunks.")
 
     print(f'Changed {change_count} blocks')
 
     # Save to a file
-    path = 'original_copy/'
-    filename = 'r.'+str(rX)+'.'+str(rZ)+'.mca'
-    newRegion.save(path + filename)
+    #path = 'original_copy/'
+    #filename = 'r.'+str(rX)+'.'+str(rZ)+'.mca'
+    newRegion.save(target_dir + "/" + filename)
 
 ###################################################################################################
 # Main
 ###################################################################################################
-def run(progress, chunk_label, files_label, water_blocks, air_blocks, new_blocks):
+def run(src_dir,
+    chunk_progress, file_progress,
+    chunk_label, files_label,
+    water_blocks, air_blocks, new_blocks):
 
     if (water_blocks.get() == 1):
         print("Water Blocks are getting fixed!")
@@ -184,19 +198,35 @@ def run(progress, chunk_label, files_label, water_blocks, air_blocks, new_blocks
     else:
         print("New Blocks are not getting fixed!")
 
-
-    # TODO for each file do stuff
-    # TODO use second progress bar
     print('.. starting')
     print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-    copyRegion(progress, chunk_label, 0, 0)
+
+    filelist = os.listdir(src_dir)
+    if len(filelist) == 0:
+        print("No files found! Select a different source path.")
+        return
+
+    try:
+        target_dir = src_dir + "_copy"
+        if not os.path.exists(target_dir):
+            os.mkdir(target_dir)
+    except OSError:
+        print ("Creation of the directory %s failed" % target_dir)
+    else:
+        print ("Successfully created the directory %s " % target_dir)
+
+    file_progress["maximum"] = len(filelist)
+    files_label.config(text=f"Finished file {0} of {len(filelist)} files.")
+
+    i = 1
+    for filename in filelist:
+        if filename.endswith(".mca"):
+            copyRegion(chunk_progress, chunk_label, src_dir, target_dir, filename)
+        else:
+            continue
+        updateProgressBar(file_progress, i)
+        files_label.config(text=f"Finished file {i} of {len(filelist)} files.")
+        i += 1
+
     print('.. finished')
     print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-
-    files_label.config(text=f"Finished file {1} of {1} files.")
-
-    # max = 11
-    # for i in range(max):
-    #     updateProgressBar(progress, i * 10)
-    #     chunk_label.config(text=f"Finished chunk {i} of {max-1} chunks.")
-    #     files_label.config(text=f"Finished file {i} of {max-1} files.")
